cb2a574ea36aaac211f4922819605cd20f940b04
evanboylan <eboylan09@gmail.com>
2021-09-21 13:09:41 -0400

    Virtual Pet Completed

--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -3,7 +3,6 @@ package virtual_pet;
 import java.util.Scanner;
 
 public class VirtualPetApplication {
-    //commenting to commit
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLUE = "\u001B[34m";
